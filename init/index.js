6669e5c7392e273b0d6d48242630be184207c456
sukritiambasta <beliebersukriti98@gmail.com>
2026-05-20 06:54:53 +0530

    fixed bug2

--- init/index.js
+++ init/index.js
@@ -1,25 +1,25 @@
-const mongoose =require("mongoose");
+const mongoose = require("mongoose");
 const initdata = require("./data.js");
-const Listing=require("../models/listing.js");
+const Listing = require("../models/listing.js");
 
 
 
-const MONGO_URL ="mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = "mongodb+srv://suku:suku123@cluster0.jjl0afc.mongodb.net/wanderlust?retryWrites=true&w=majority&appName=Cluster0";
 
 
 main().then(() => {
-console.log("connected to DB");
-}).catch(err=>{
-    console.log("err");
+    console.log("connected to DB");
+}).catch(err => {
+    console.log("Database connection error:", err);
 });
 async function main() {
     await mongoose.connect(MONGO_URL);
-    
+
 }
 
-const initDB= async() =>{
+const initDB = async () => {
     await Listing.deleteMany({});
-    initdata.data=initdata.data.map((obj)=>({...obj,owner:"6873cd1608dcc1774af4e0f0"}));
+    initdata.data = initdata.data.map((obj) => ({ ...obj, owner: "6873cd1608dcc1774af4e0f0", category: "Trending" }));
     await Listing.insertMany(initdata.data);
     console.log("data  was initialized");
 };
